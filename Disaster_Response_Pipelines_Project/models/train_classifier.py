76f76035e995dc77a5cccba6a6c4d6c428c35729
Hardik Chawla <43451981+hchawla94@users.noreply.github.com>
2019-06-13 15:57:28 +0530

    Add files via upload

--- Disaster_Response_Pipelines_Project/models/train_classifier.py
+++ Disaster_Response_Pipelines_Project/models/train_classifier.py
@@ -78,11 +78,11 @@ def build_model():
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer = tokenize)),
         ('tfidf', TfidfTransformer()),
-        ('clf', MultiOutputClassifier(AdaBoostClassifier()))])
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))])
 
     parameters = {'vect__min_df': [1, 5],
-                  "clf__estimator__learning_rate" : [1.0, 0.5],
-                  "clf__estimator__n_estimators" : [20,50]}
+              'clf__estimator__max_depth': [10, 50],
+              'clf__estimator__min_samples_leaf':[1, 10]}
     
     cv = GridSearchCV(pipeline, param_grid=parameters)
     
